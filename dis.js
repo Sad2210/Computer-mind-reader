e5aa377a2a6c48b9db863341c3ba5c231954acd0
Samshul Ansari <128343150+Sad2210@users.noreply.github.com>
2025-10-21 22:44:28 +0530

    Add files via upload

---
+++ dis.js
@@ -0,0 +1,7 @@
+<script type="module">
+  import { sdk } from 'https://esm.sh/@farcaster/miniapp-sdk'
+</script>
+import { sdk } from '@farcaster/miniapp-sdk'
+ 
+// After your app is fully loaded and ready to display
+await sdk.actions.ready()
