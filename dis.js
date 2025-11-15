a22efee2ee14a55345b364e918c4adb70dac100f
Sad <128343150+Sad2210@users.noreply.github.com>
2025-11-15 14:09:52 +0530

    Refactor import statement for miniapp-sdk

--- dis.js
+++ dis.js
@@ -1,6 +1,3 @@
-<script type="module">
-  import { sdk } from 'https://esm.sh/@farcaster/miniapp-sdk'
-</script>
 import { sdk } from '@farcaster/miniapp-sdk'
  
 // After your app is fully loaded and ready to display
